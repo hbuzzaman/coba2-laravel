a882b67e5a1fc3fa9c0da46d0c7a00d6f0ca7d75
hbuzzaman <hbuzzaman@gmail.com>
2022-07-01 09:51:15 +0700

    policy

---
+++ app/Http/Controllers/HahaaController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\hahaa;
+use App\Http\Requests\StorehahaaRequest;
+use App\Http\Requests\UpdatehahaaRequest;
+
+class HahaaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorehahaaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorehahaaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\hahaa  $hahaa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(hahaa $hahaa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\hahaa  $hahaa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(hahaa $hahaa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatehahaaRequest  $request
+     * @param  \App\Models\hahaa  $hahaa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatehahaaRequest $request, hahaa $hahaa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\hahaa  $hahaa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(hahaa $hahaa)
+    {
+        //
+    }
+}
